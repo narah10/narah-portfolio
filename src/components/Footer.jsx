5b04fd51e7a6f67e59033799f4966f24a8b27a84
Na Rah Hernandez <90667258+narah10@users.noreply.github.com>
2025-04-07 19:50:13 -0700

    Fixing success page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <div className="w-ful mt-10 mb-5">
+        <div className="w-full mt-10 mb-5">
             <footer className="mx-auto max-w-4xl text-accentDarkColor">© Copyright 2025 Na Rah Hernandez Portfolio</footer>
         </div>
 );
--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -1,7 +1,7 @@
 export default function Success() {
     return (
-        <div>
-            <h1 className="mb-4 text-5xl tracking-tight font-extrabold text-white">Thank you!</h1>
+        <div className="bg-accentDarkColor w-1/2 rounded-lg p-10 mx-auto my-20">
+            <h1 className="mb-4 text-5xl tracking-tight font-extrabold text-accentColor">Thank you!</h1>
             <p className="mb-4 text-white">Your message has been sent successfully and I will reach out to you as soon as I can.</p>
         </div>
     );
